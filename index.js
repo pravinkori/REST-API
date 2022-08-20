e09d5891fcef512dcd7b3adcfdc9945f30593c0c
Pravin <pravinkori@protonmail.ch>
2022-08-20 21:45:50 +0530

    Route parameter and query string implementation.

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ app.get('/', (req, res) => {
     res.send("Hello World!!");
 });
 
+// This endpoint is to get all the courses
 app.get('/api/courses', (req, res) => {
     res.send(["Java", "JavaScript", "Python"]);
 });
+
+// This endpoint is to get single courses by id with the help of Route parameter
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/api/courses/:course/:id', (req, res) => {
+    res.send(req.params)
+})
+
+// Query string parameter
+app.get('/api/courses/:course/:id', (req, res) => {
+    res.send(req.query)
+})
 
 const port = process.env.PORT || 3000;
 
